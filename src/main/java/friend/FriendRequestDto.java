719f406c5fe23b486f080c655397baf510b97698
Maciej Radzikowski <mradzikowski@blastlab.co>
2018-11-24 21:05:18 +0100

    Find and notify friend endpoints

---
+++ src/main/java/friend/FriendRequestDto.java
@@ -0,0 +1,13 @@
+package friend;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FriendRequestDto {
+
+	private String username;
+}
